28f1222a30c2497ed1d73dfc9240bdb5442a3f8e
Victor Garcia Mestre <vgmestre@gmail.com>
2025-10-19 20:41:35 +0200

    feat: date time inputs

--- src/components/period-picker.tsx
+++ src/components/period-picker.tsx
@@ -18,7 +18,7 @@ import {
   SelectValue,
 } from "./ui/select";
 import { DateRange } from "react-day-picker";
-import { isSameSecond, subDays } from "date-fns";
+import { isAfter, isSameSecond } from "date-fns";
 import { cn } from "@/lib/utils";
 import { Input } from "./ui/input";
 
@@ -47,6 +47,11 @@ function isValidDate(date: Date | undefined) {
   return !isNaN(date.getTime());
 }
 
+function isValidTime(time: string): boolean {
+  const timeRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+  return timeRegex.test(time);
+}
+
 function formatTime(date: Date | undefined) {
   if (!date) {
     return "";
@@ -70,6 +75,90 @@ interface Props {
   value: DateRange;
   onValueChange: (range: DateRange) => void;
   presets: Preset[];
+}
+
+interface FormValues {
+  startValue: string;
+  startTime: string;
+  endValue: string;
+  endTime: string;
+}
+
+type FormErrors = Record<keyof FormValues, string | null>;
+
+type ParseDateTimesReturnValue =
+  | {
+      data: {
+        startDate: Date;
+        endDate: Date;
+      };
+      errors: undefined;
+      success: true;
+    }
+  | {
+      data: undefined;
+      errors: {
+        startValue: string | null;
+        endValue: string | null;
+        startTime: string | null;
+        endTime: string | null;
+      };
+      success: false;
+    };
+
+function parseFormValues({
+  startValue,
+  startTime,
+  endValue,
+  endTime,
+}: FormValues): ParseDateTimesReturnValue {
+  const errors: FormErrors = {
+    startValue: null,
+    endValue: null,
+    startTime: null,
+    endTime: null,
+  };
+
+  if (!isValidDate(new Date(startValue))) {
+    errors.startValue = "Invalid date format";
+  }
+  if (!isValidDate(new Date(endValue))) {
+    errors.endValue = "Invalid date format";
+  }
+  if (!isValidTime(startTime)) {
+    errors.startTime = "Invalid time format";
+  }
+  if (!isValidTime(endTime)) {
+    errors.endTime = "Invalid time format";
+  }
+
+  if (
+    isAfter(
+      new Date(startValue).setTime(new Date(startTime).getTime()),
+      new Date(endValue).setTime(new Date(endTime).getTime()),
+    )
+  ) {
+    errors.endValue = "Invald date format";
+  }
+
+  const hasErrors = Object.values(errors).some((value) => value !== null);
+
+  if (hasErrors) {
+    return {
+      data: undefined,
+      errors,
+      success: false,
+    };
+  }
+
+  const startDate = new Date(`${startValue} ${startTime}`);
+  const endDate = new Date(`${endValue} ${endTime}`);
+
+  return {
+    data: { startDate, endDate },
+    errors: undefined,
+    success: true,
+  };
 }
 
 function isValidRange(
@@ -111,77 +200,112 @@ const InputPicker: React.FC<{
   defaultValue: DateRange;
   onApplyChanges: (range: DateRange) => void;
 }> = ({ defaultValue, onApplyChanges }) => {
-  const [startValue, setStartValue] = React.useState<string>(
-    defaultValue.from === undefined ? "" : formatDate(defaultValue.from),
-  );
-  const [startTime, setStartTime] = React.useState<string>(
-    defaultValue.from === undefined ? "" : formatTime(defaultValue.from),
-  );
-  const [endDate, setEndDate] = React.useState<string>(
-    defaultValue.to === undefined ? "" : formatDate(defaultValue.to),
-  );
-  const [endTime, setEndTime] = React.useState<string>(
-    defaultValue.to === undefined ? "" : formatTime(defaultValue.to),
-  );
+  const [showError, setShowError] = React.useState<boolean>(false);
+
+  const [formData, setFormData] = React.useState<FormValues>({
+    startValue:
+      defaultValue.from === undefined ? "" : formatDate(defaultValue.from),
+    startTime:
+      defaultValue.from === undefined ? "" : formatTime(defaultValue.from),
+    endValue: defaultValue.to === undefined ? "" : formatDate(defaultValue.to),
+    endTime: defaultValue.to === undefined ? "" : formatTime(defaultValue.to),
+  });
+
+  const { data, errors, success } = parseFormValues(formData);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!success) {
+      setShowError(true);
+      return;
+    }
+
+    onApplyChanges({
+      from: data.startDate,
+      to: data.endDate,
+    });
+  };
+
+  const handlePartialChange = (values: Partial<FormValues>) => {
+    setFormData({ ...formData, ...values });
+  };
 
   return (
-    <div className="space-y-2 px-3 py-2.5">
+    <form className="space-y-2 px-3 py-2.5" onSubmit={onSubmit}>
       <div className="flex flex-col">
-        <span className="text-muted-foreground mb-1 text-xs">Start</span>
+        <div className="flex items-center justify-between">
+          <span className="text-muted-foreground mb-1 text-xs">Start</span>
+          {showError &&
+            (errors?.startValue !== undefined ||
+              errors?.startTime !== undefined) && (
+              <span className="text-destructive text-xs">
+                {errors.startValue ?? errors.startTime}
+              </span>
+            )}
+        </div>
         <div className="flex items-center gap-2">
           <Input
             id="date"
-            value={startValue}
+            value={formData.startValue}
             placeholder="June 01, 2025"
             className="bg-background"
             onChange={(e) => {
-              setStartValue(e.target.value);
+              handlePartialChange({ startValue: e.target.value });
             }}
+            aria-invalid={showError && !!errors?.startValue}
           />
           <Input
-            type="time"
+            type="text"
             id="time-picker"
-            value={startTime}
+            value={formData.startTime}
             onChange={(e) => {
-              setStartTime(e.target.value);
+              handlePartialChange({ startTime: e.target.value });
             }}
             className="bg-background w-24 shrink-0 appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+            aria-invalid={showError && !!errors?.startTime}
           />
         </div>
       </div>
 
       <div className="flex flex-col">
-        <span className="text-muted-foreground mb-1 text-xs">End</span>
+        <div className="flex items-center justify-between">
+          <span className="text-muted-foreground mb-1 text-xs">End</span>
+          {showError &&
+            (errors?.endValue !== undefined ||
+              errors?.endTime !== undefined) && (
+              <span className="text-destructive text-xs">
+                {errors?.endValue ?? errors?.endTime}
+              </span>
+            )}
+        </div>
         <div className="flex items-center gap-2">
           <Input
             id="date"
-            value={endDate}
+            value={formData.endValue}
             placeholder="June 01, 2025"
             className="bg-background"
             onChange={(e) => {
-              setEndDate(e.target.value);
+              handlePartialChange({ endValue: e.target.value });
             }}
+            aria-invalid={showError && !!errors?.endValue}
           />
           <Input
-            type="time"
+            type="text"
             id="time-picker"
-            value={endTime}
+            value={formData.endTime}
             onChange={(e) => {
-              setEndTime(e.target.value);
+              handlePartialChange({ endTime: e.target.value });
             }}
             className="bg-background w-24 shrink-0 appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+            aria-invalid={showError && !!errors?.endTime}
           />
         </div>
       </div>
-      <Button
-        variant="outline"
-        className="w-full"
-        size="sm"
-        onClick={onApplyChanges}
-      >
+      <Button variant="outline" className="w-full" size="sm" type="submit">
         Apply ↵
       </Button>
-    </div>
+    </form>
   );
 };
 
